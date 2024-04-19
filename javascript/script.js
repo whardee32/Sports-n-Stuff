4d6492965a1e2af3de007b2f7531502d546026fa
whardee32 <willhardee32@gmail.com>
2024-04-18 22:02:49 -0600

    added login page functionality

--- javascript/script.js
+++ javascript/script.js
@@ -59,30 +59,13 @@ function printTeams(teamData) {
     
 }
 
-// Function to handle sign-in submission
-function signIn() {
-    // Get the username from the form
-    var username = document.getElementById("username").value;
-    // Assuming authentication is successful, you would typically store the username in a session or cookie
-    // For demonstration purposes, I'll just use localStorage to store the username temporarily
-    localStorage.setItem("loggedInUsername", username);
-    // Replace "Account" navigation link with the signed-in username
-    var accountLink = document.querySelector('.nav-item1 a[href="signin.html"]');
-    if (accountLink) {
-        accountLink.innerHTML = '<span class="navtext">' + username + '</span>';
-        accountLink.href = '#'; // Optionally, change the link target
-    }
-}
+const navText1 = document.querySelector('.navtext1');
 
-// Check if the user is already signed in
-window.onload = function () {
-    var loggedInUsername = localStorage.getItem("loggedInUsername");
-    if (loggedInUsername) {
-        // Replace "Account" navigation link with the signed-in username
-        var accountLink = document.querySelector('.nav-item1 a[href="signin.html"]');
-        if (accountLink) {
-            accountLink.innerHTML = '<span class="navtext">' + loggedInUsername + '</span>';
-            accountLink.href = '#'; // Optionally, change the link target
-        }
-    }
-};
+// Get username from cookie
+const cookieValue = document.cookie.split('; ').find(cookie => cookie.startsWith('username='));
+const username = cookieValue ? cookieValue.split('=')[1] : '';
+
+if (username) {
+  navText1.textContent = username;
+  navText1.classList.add('underline'); // Add a class for underlining
+}
